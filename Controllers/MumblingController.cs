4f52afc92beb66dad054375feee6a53c54aaf344
Karl Frick <karl.frick5@gmail.com>
2021-05-05 16:07:16 -0400

    Completed Mumbling

---
+++ Controllers/MumblingController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExplosionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MumblingController : ControllerBase
+    {
+        [HttpGet]
+        public String Accum(string s)
+        {
+            var accumString = "";
+            var charList = new List<char>(s);
+            for (var i = 0; i < charList.Count; i++)
+            {
+                for (var j = 0; j <= i; j++)
+                {
+                    if (j == 0)
+                    {
+                        accumString += char.ToUpper(charList[i]);
+                    }
+                    else
+                    {
+                        accumString += char.ToLower(charList[i]);
+                    }
+                }
+                if (i != charList.Count - 1)
+                {
+                    accumString += "-";
+                }
+            }
+            return accumString;
+
+
+        }
+    }
+}
